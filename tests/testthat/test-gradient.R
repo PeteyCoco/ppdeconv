bb39748d212f29e820166348102f49b26131887b
PeteyCoco <peter.carsten.collins@gmail.com>
2022-01-24 09:46:55 -0500

    Combined gradient and hessian into derivs

--- tests/testthat/test-gradient.R
+++ tests/testthat/test-gradient.R
@@ -31,10 +31,12 @@ test_that("gradient functions works in bdiag case", {
   sdot <- sdot(a = a, y = y_bdiag, Q = Q_bdiag, P = P_bdiag, S = S_bdiag, c0 = c0)
   grad <- gradient(a = a, y = y_bdiag, Q = Q_bdiag, P = P_bdiag, S = S_bdiag, c0 = c0)
 
+  # Check the length of the gradients
   expect_length(ldot, 20)
   expect_length(sdot, 20)
   expect_length(grad, 20)
 
+  # The gradients should be symettric
   expect_equal(ldot[1:10], ldot[11:20])
   expect_equal(sdot[1:10], sdot[11:20])
   expect_equal(grad[1:10], grad[11:20])
