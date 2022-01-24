c0e61aec86c7efe47bed31417866a45dd30ac141
PeteyCoco <peter.carsten.collins@gmail.com>
2022-01-24 12:51:18 -0500

    ADD to_bdag
    Added the function to_bdiag, which converts the user defined list of
    data for use in ppdeconv from a list format to a block-diagonal format.

--- R/utils.R
+++ R/utils.R
@@ -22,3 +22,26 @@ get_midpoints <- function(x_breaks){
   mid <- (x_breaks[-1] + x_breaks[-length(x_breaks)])/2
   return(mid)
 }
+
+#' Put data into block diagonal format
+#'
+#' @param data list of data for the ppdeconv function
+#'
+#' @return list of data in block diagonal format
+#' @export
+#'
+#' @examples
+#' # TODO
+to_bdiag <- function(data){
+
+    # Put data into block diagonal format
+    y_bdiag <- unlist(lapply(data, function(x) x$y))
+    Q_bdiag <- bdiag(lapply(data, function(x) x$Q))
+    P_bdiag <- bdiag(lapply(data, function(x) x$P))
+    S_bdiag <- bdiag(lapply(data, function(x) x$S))
+    c0_bdiag <- rep(unlist(lapply(data, function(x) x$c0)),
+                   times = unlist(lapply(data, function(x) nrow(x$S))))
+
+    data_bdiag <- list(y = y_bdiag, Q = Q_bdiag, P = P_bdiag, S = S_bdiag, c0 = c0_bdiag)
+    return(data_bdiag)
+}
