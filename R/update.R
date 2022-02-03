999264cd0f65b759092b706c2f0572d724aa794c
PeteyCoco <peter.carsten.collins@gmail.com>
2022-02-03 17:11:08 -0500

    Rename update functions

--- R/update.R
+++ R/update.R
@@ -1,9 +1,10 @@
-update_a <- function(x, a){
+update_Fix <- function(x, a){
   x$a <- a
   x
 }
 
-update_b <- function(x, b){
+update_Var <- function(x, a, b){
+  x$a <- a
   x$b <- b
   x$P <- x$P_fn(x)
   x
