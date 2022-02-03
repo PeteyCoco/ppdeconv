3885cd0c56f3a931c1dca7ffbf11df1342ffb76e
PeteyCoco <peter.carsten.collins@gmail.com>
2022-02-03 13:20:07 -0500

    Change get_midpoints
    Added an optional parameter for computing q midpoints.
    This will be used in the quadrature of P_hnorm and P_exp.

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,10 @@
 #' Get midpoint
 #'
-#' A helper function for computing the midpoints for a given a vector defining a partition of an interval.
+#' A helper function that returns a vector of q equally spaced
+#' points within the intervals specified by x_breaks
 #'
 #' @param x_breaks a (n+1) vector defining a partition of an interval
+#' @param q an integer specifying the number of points within interval
 #'
 #' @return an n vector of the midpoints of `x_breaks`
 #' @export
@@ -10,13 +12,20 @@
 #' @examples
 #' x_breaks <- c(0, 2, 3, 5, 8)
 #' x_mid <- get_midpoints(x_breaks)
-get_midpoints <- function(x_breaks) {
+get_midpoints <- function(x_breaks, q = 1) {
   assertthat::assert_that(length(x_breaks) >= 2,
                           is.numeric(x_breaks),
                           assertthat::noNA(x_breaks))
 
   x_breaks <- sort(x_breaks)
-  mid <- (x_breaks[-1] + x_breaks[-length(x_breaks)]) / 2
+  x_diff <- diff(x_breaks)
+  mid <-
+    lapply(
+      1:q,
+      FUN = function(x)
+        x_breaks[-length(x_breaks)] + as.numeric(x) * x_diff / (q + 1)
+    )
+  mid <- sort(unlist(mid))
   return(mid)
 }
 
