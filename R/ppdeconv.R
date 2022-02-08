227ef411c017d4758ef83a9603c2be504807f239
PeteyCoco <peter.carsten.collins@gmail.com>
2022-02-08 14:41:17 -0500

    Change output of ppdeconv
    Now ppdeconv outputs a ppdeconvFit object, whose methods are yet
    to be defined.

--- R/ppdeconv.R
+++ R/ppdeconv.R
@@ -14,6 +14,9 @@ ppdeconv <-
            data,
            mode = "fixed",
            method = "mle") {
+    # If program doesn't work, return NULL
+    fit <- NULL
+
     # Convert list of data to a list of ppdeconvFix or ppdeconvVar objects
     if (mode == "fixed") {
       data <- lapply(
@@ -38,26 +41,44 @@ ppdeconv <-
     data <- configure_idx(data, mode = mode)
 
     if (method == "mle") {
-
       # Define the objective function and its gradient
       # (I'm updating the parameters within each function to make sure
       # that the most recent parameters are used in both functions)
       fn <- function(p) {
-        data <- lapply(data, FUN = function(x) set_par(x, p))
-        result <- lapply(data, FUN = function(x) get_loglik(x, p))
+        data <- lapply(
+          data,
+          FUN = function(x)
+            set_par(x, p)
+        )
+        result <- lapply(
+          data,
+          FUN = function(x)
+            get_loglik(x, p)
+        )
         sum(unlist(result))
       }
       gr <- function(p) {
-        data <- lapply(data, FUN = function(x) set_par(x, p))
-        result <- lapply(data, FUN = function(x) get_gradient(x, p))
+        data <- lapply(
+          data,
+          FUN = function(x)
+            set_par(x, p)
+        )
+        result <- lapply(
+          data,
+          FUN = function(x)
+            get_gradient(x, p)
+        )
         rowSums(matrix(unlist(result), ncol = length(data)))
+      }
 
 
       # Set the initial parameter guess
       if (is.null(a0) & mode == "fixed") {
-        a0 <- unlist(lapply(data, function(x) rep(0, length(x$a))))
+        a0 <- unlist(lapply(data, function(x)
+          rep(0, length(x$a))))
       } else if (is.null(a0) & mode == "variable") {
-        a0 <- rep(0, max(unlist(lapply(data, function(x) x$b_idx))))
+        a0 <- rep(0, max(unlist(lapply(data, function(x)
+          x$b_idx))))
       }
 
       fit <- stats::optim(
@@ -67,11 +88,9 @@ ppdeconv <-
         method = "BFGS",
         control = list(fnscale = -1)
       )
-    }
-    else if (method == "rmle") {
+    } else if (method == "rmle") {
       stop("mode 'reml' has not been implemented")
-    }
-    else{
+    } else{
       stop("mode must be either 'mle' or 'reml'")
     }
 
