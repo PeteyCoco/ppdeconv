40e2b7e8fe54154645882aa534cec2bf4a6a1d4a
PeteyCoco <peter.carsten.collins@gmail.com>
2022-02-08 14:43:12 -0500

    Fix style formatting

--- R/sim_nhpp.R
+++ R/sim_nhpp.R
@@ -14,28 +14,33 @@
 #' y <- sim_nhpp(lambda = lambda, min = 0, max = 10, M = 1)
 #' # Using unknown bound M over interval [0,10]
 #' y <- sim_nhpp(lambda = lambda, min = 0, max = 10, M = NULL)
-sim_nhpp <- function(lambda, min, max, M = NULL){
-
+sim_nhpp <- function(lambda, min, max, M = NULL) {
   assertthat::assert_that(assertthat::is.number(min),
                           assertthat::is.number(max),
                           max > min)
 
-  if(is.null(M)){
-    M <- stats::optimize(f = lambda, lower = min, upper = max, maximum = TRUE)[["objective"]]
-    M <- M *(1.01) # Ensure that M is an upper bound of lambda
+  if (is.null(M)) {
+    M <-
+      stats::optimize(
+        f = lambda,
+        lower = min,
+        upper = max,
+        maximum = TRUE
+      )[["objective"]]
+    M <- M * (1.01) # Ensure that M is an upper bound of lambda
   }
   assertthat::assert_that(M > 0,
                           msg = "M must be a positive number. If M = NULL, check that lambda is a nonnegative function.")
 
   # Simulate from HPP with rate M
   width <- max - min
-  hpp <- ppdeconv::sim_hpp(rate=M, min = min, max = max)
+  hpp <- ppdeconv::sim_hpp(rate = M, min = min, max = max)
 
   assertthat::assert_that(all(lambda(hpp) >= 0),
                           msg = "Some values of lambda are negative. Check that lambda is a nonnegative function.")
 
   # Importance sampling
-  p <- lambda(hpp)/M
+  p <- lambda(hpp) / M
   keep <- stats::rbinom(n = length(hpp), size = 1, p = p)
 
   return(hpp[as.logical(keep)])
