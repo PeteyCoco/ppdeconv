0bb4a78c35f52bf05cf8079da04a1854e26ac48d
PeteyCoco <peter.carsten.collins@gmail.com>
2022-02-16 09:50:14 -0500

    Rewrite NAMESPACE

--- R/set_par.R
+++ R/set_par.R
@@ -1,16 +1,52 @@
+#' Title
+#'
+#' @param x
+#' @param p
+#'
+#' @return
+#' @export
+#'
+#' @examples
 set_par <- function(x, p){
   UseMethod("set_par")
 }
 
+#' Title
+#'
+#' @param x
+#' @param p
+#'
+#' @return
+#' @export
+#'
+#' @examples
 set_par.default <- function(x, p){
   stop(paste0("no method 'set_par' for class", class(x)))
 }
 
+#' Title
+#'
+#' @param x
+#' @param p
+#'
+#' @return
+#' @export
+#'
+#' @examples
 set_par.ppdeconvFix <- function(x, p){
   x$a <- p[x$a_idx]
   x
 }
 
+#' Title
+#'
+#' @param x
+#' @param p
+#'
+#' @return
+#' @export
+#'
+#' @examples
 set_par.ppdeconvVar <- function(x, p){
   x$a <- p[x$a_idx]
   x$b <- p[x$b_idx]
